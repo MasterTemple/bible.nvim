61010d64ee350420c53957d3e009f12064b66235
Blake Scampone <blake.scampone@gmail.com>
2023-08-18 17:58:40 -0700

    More testing

--- lua/telescope/_extensions/bible.lua
+++ lua/telescope/_extensions/bible.lua
@@ -1,35 +1,8 @@
-local entry_display = require("telescope.pickers.entry_display")
-local finders = require("telescope.finders")
-local pickers = require("telescope.pickers")
-local conf = require("telescope.config").values
--- local reference = require("reference")
+local telescope = require('telescope')
+local my_extension = require('telescope._extensions.bible.bible')
 
-local function prepare_results()
-    local next = {}
-    for idx = 1, 20 do
-        -- if list[idx].filename ~= "" then
-        --     list[idx].index = idx
-        --     table.insert(next, list[idx])
-        -- end
-				table.insert(next, idx)
-    end
-
-    return next
-end
-
-local generate_new_finder = function()
-    return finders.new_table({
-        results = prepare_results()
-    })
-end
-
-return function(opts)
-    opts = opts or {}
-
-    pickers.new(opts, {
-        prompt_title = "bible",
-        finder = generate_new_finder(),
-        sorter = conf.generic_sorter(opts),
-        previewer = conf.grep_previewer(opts)
-    }):find()
-end
+return telescope.register_extension({
+  exports = {
+    my_extension = my_extension,
+  }
+})
